5b3436f77b8f9894a934b3f7300748e1cfb4f8bc
Dennis Parker <dennis@windhavengroup.com>
2025-12-10 12:52:40 -0600

    removed all the phase1 code, not the tests yet though

--- src/palaver/commands/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Command document system for flexible speech workflows."""
-
-from palaver.commands.speech_bucket import SpeechBucket
-from palaver.commands.command_doc import CommandDoc
-from palaver.commands.simple_note import SimpleNote
-
-__all__ = ["SpeechBucket", "CommandDoc", "SimpleNote"]
--- src/palaver/config/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Configuration module for Palaver recorder."""
-
-from palaver.config.recorder_config import RecorderConfig
-
-__all__ = ["RecorderConfig"]
